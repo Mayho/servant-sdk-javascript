d3c8a77804768aea4b281599e6678106a5bc385a
Austen Collins <austencollins@gmail.com>
2014-11-12 15:53:55 -0800

    Fix nested archetypes

--- src/servant_sdk_javascript.js
+++ src/servant_sdk_javascript.js
@@ -368,8 +368,11 @@
     };
 
     Servant._validateNestedArchetype = function(errors, rules, value) {
-        if (this._utilities.whatIs(value) !== 'object') return 'Invalid type - Nested Archetype must be an object';
-        if (!value._id || typeof value._id === 'undefined') return 'Nested Archetypes must be published on Servant first.  Please publish this nested Archetype on Servant, then include the publshed object'
+        if (this._utilities.whatIs(value) === 'object') {
+            if (!value._id || typeof value._id === 'undefined') return 'Nested Archetypes must be published on Servant first.  Please publish this nested Archetype on Servant, then include the publshed object';
+        } else if (this._utilities.whatIs(value) !== 'string') {
+            return 'Nested Archetypes must either be an ID string or an object that has already been published on Servant and has an "_id" property.';
+        }
         return null;
     };
 
